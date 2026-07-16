63ec246a184f9564fec8a8ecea759f92fdfc9dcd
OmkarM9090 <omkarmahadik180@gmail.com>
2026-07-16 20:46:57 +0530

    created toggle logic

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import './i18n'
 import App from './App.jsx'
 import { LanguageProvider } from './context/LanguageContext.jsx'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LanguageProvider>
-      <Suspense fallback={<div className="flex h-screen items-center justify-center">Loading...</div>}>
-        <App />
-      </Suspense>
-    </LanguageProvider>
+    <ThemeProvider>
+      <LanguageProvider>
+        <Suspense fallback={<div className="flex h-screen items-center justify-center">Loading...</div>}>
+          <App />
+        </Suspense>
+      </LanguageProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
